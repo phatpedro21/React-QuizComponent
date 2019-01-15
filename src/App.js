fb4ced5c064cdba6a3a26694871a9fe088a97da4
Peter <Peter.Maloney@fdmgroup.com>
2019-01-15 11:23:07 +0000

    Module 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Quiz from "./Quiz"
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <div>
+        <Quiz />
+      </div>
+      
     )
   }
 }
